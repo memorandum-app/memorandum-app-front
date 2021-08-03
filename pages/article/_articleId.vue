6da5502206588ddc1255e72a5ece821bb4d42a76
Tagami-Yushin <spitzman48@gmail.com>
2021-08-04 01:41:39 +0900

    test

--- pages/article/_articleId.vue
+++ pages/article/_articleId.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="article-main-container">
+        <div class="related-articles">
+            <div id="related-title">
+                <h2>関連記事</h2>
+            </div>
+            <div v-for="(articleListItem,index) in articleList" v-bind:key="articleListItem.articleId">
+                <div id="related-articles-object" v-if="index < 5">
+                    <h3>{{articleListItem.articleTitle}}</h3>
+                    <h5>by {{articleListItem.author}}</h5>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -32,6 +43,29 @@ export default {
 
 <style lang="scss" scoped>
 .article-main-container {
-    
+    height: auto;
+    & .related-articles {
+        background-color: Red;
+        width: 60%;
+        margin: 0 auto;
+
+        & #related-title {
+            padding-left: 10px;
+            font-size: 30px;
+            line-height: 0px;
+        }
+        & #related-articles-object{
+            border-top: 2px solid black;
+            line-height: 0px;
+            & h3{
+                padding-left: 20px;
+                font-size: 28px;
+            }
+            & h5{
+                padding-left: 30px;
+                font-size: 15px;
+            }
+        }
+    }
 }
 </style>
