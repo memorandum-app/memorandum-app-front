a51f539b48b6f25cff530bfe83d70db9110c7cbf
Tagami-Yushin <spitzman48@gmail.com>
2021-08-06 01:14:58 +0900

    comit

--- pages/article/_articleId.vue
+++ pages/article/_articleId.vue
@@ -43,28 +43,64 @@ export default {
 
 <style lang="scss" scoped>
 .article-main-container {
-    height: auto;
+    background-color: gray;
+    height:auto;
+
     & .related-articles {
-        background-color: Red;
-        width: 60%;
+        background-color: white;
+        width: 70%;
         margin: 0 auto;
+        border-top: 1px ;
 
         & #related-title {
-            padding-left: 10px;
-            font-size: 30px;
-            line-height: 0px;
+            position: relative;
+            & h2 {
+                height: auto;
+                margin: 0 auto;
+                padding-top: 3px;
+                padding-left: 10px;
+                font-size: 32px;
+                font-weight: normal;
+                cursor: default;
+            }
         }
         & #related-articles-object{
-            border-top: 2px solid black;
-            line-height: 0px;
+            position: relative;
+            height: auto;
             & h3{
-                padding-left: 20px;
+                display: inline-block;
+                margin: 0 auto;
+                padding: 8px 0px 2px 20px;
                 font-size: 28px;
+                font-weight: normal;
+
+                &:hover {
+                    cursor: pointer;
+                    text-decoration: underline;
+                }
             }
-            & h5{
-                padding-left: 30px;
+            & h5 {
+                margin: 0 auto;
+                padding: 0px 0px 3px 30px;
                 font-size: 15px;
+                font-weight: normal;
             }
+            &:hover {
+                background-color: rgb(228, 228, 228);
+                box-shadow: 0 4px 7px 0 rgba(0, 0, 0, 0.5);
+                transform: translateY(-1px);
+                transition: all 0.3s ease 0s;
+            }
+            &::before {
+                position: absolute;
+                content: "";
+                background-color: black;
+                width: 100%;
+                height: 1px;
+                left: 0;
+                box-shadow: 0px 0.5px;
+            }
+            
         }
     }
 }
